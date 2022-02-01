e4f6fee535926211846c7645bf30b9180d4b5486
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2022-02-01 20:44:27 +0100

    set-up

---
+++ src/components/ProjectOverview.js
@@ -0,0 +1,12 @@
+import {React} from "react";
+
+export const ProjectOverview = (props) => {
+    return (
+        <div>
+            <h2>ProjectTitlte</h2>
+            <h2>ProjectLocation</h2>
+        </div>
+    )
+}
+
+export default ProjectOverview;
